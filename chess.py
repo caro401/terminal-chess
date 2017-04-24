d62975afc9ef801dc4449e30b85b0dd3a36c66e8
Caro Appleby <caroline.appleby@cantab.net>
2017-04-24 12:40:31 +0100

    Draw the board in the starting position

--- chess.py
+++ chess.py
@@ -1,9 +1,17 @@
-WKING, WQUEEN, WROOK, WBISHOP, WKNIGHT, WPAWN = '♔', '♕', '♖', '♗', '♘', '♙'
-BKING, BQUEEN, BROOK, BBISHOP, BKNIGHT, BPAWN = '♚', '♛', '♜', '♝', '♞', '♟'
+WK, WQ, WR, WB, WN, WP = '♔', '♕', '♖', '♗', '♘', '♙'
+BK, BQ, BR, BB, BN, BP = '♚', '♛', '♜', '♝', '♞', '♟'
 
 
 def render_board(pieces_array=None):
+    # draw board as a string, given array of pieces
     if not pieces_array:
+        return f'{BR} {BN} {BB} {BQ} {BK} {BB} {BN} {BR}\n' + \
+               f'{BP} ' * 7 + f'{BP}\n' + \
+               '_ _ _ _ _ _ _ _\n' * 4 +\
+               f'{WP} ' * 7 + f'{WP}\n' +\
+               f'{WR} {WN} {WB} {WQ} {WK} {WB} {WN} {WR}\n'
+
+    elif len(pieces_array) != 8:
         return '_ _ _ _ _ _ _ _\n' * 8
     else:
         pass
--- test_ui.py
+++ test_ui.py
@@ -1,10 +1,25 @@
 import unittest
 import chess
 
+WK, WQ, WR, WB, WN, WP = '♔', '♕', '♖', '♗', '♘', '♙'
+BK, BQ, BR, BB, BN, BP = '♚', '♛', '♜', '♝', '♞', '♟'
+
 
 class TestUiDrawing(unittest.TestCase):
     def test_draw_empty_board(self):
-        self.assertEqual(chess.render_board(), '_ _ _ _ _ _ _ _\n' * 8)
+        self.assertEqual(chess.render_board([None]), '_ _ _ _ _ _ _ _\n' * 8)
+
+    def test_draw_start_board(self):
+        start_board = """♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
+"""
+        self.assertEqual(chess.render_board(), start_board)
 
 
 if __name__ == '__main__':

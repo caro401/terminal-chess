c5cd7d5eecf00b6827148c143d3fd19968f3af0e
Caro Appleby <caroline.appleby@cantab.net>
2017-04-24 13:35:16 +0100

    Add flag to display coordinate labels on board

--- chess.py
+++ chess.py
@@ -2,23 +2,32 @@ WK, WQ, WR, WB, WN, WP = '♔', '♕', '♖', '♗', '♘', '♙'
 BK, BQ, BR, BB, BN, BP = '♚', '♛', '♜', '♝', '♞', '♟'
 
 
-def render_board(pieces_array=None):
-    # draw board as a string, given array of pieces
-    if not pieces_array:
-        return f'{BR} {BN} {BB} {BQ} {BK} {BB} {BN} {BR}\n' + \
-               f'{BP} ' * 7 + f'{BP}\n' + \
-               '_ _ _ _ _ _ _ _\n' * 4 +\
-               f'{WP} ' * 7 + f'{WP}\n' +\
-               f'{WR} {WN} {WB} {WQ} {WK} {WB} {WN} {WR}\n'
+class Board:
+    def __init__(self, axes):
+        self.board = [[WR, WN, WB, WQ, WK, WB, WN, WR], [WP] * 8,  [None] * 8, [None] * 8,
+                      [None] * 8, [None] * 8, [BP] * 8, [BR, BN, BB, BQ, BK, BB, BN, BR]]
+        self.axes = axes
 
-    elif len(pieces_array) != 8:
-        return '_ _ _ _ _ _ _ _\n' * 8
-    else:
-        pass
+    def update_board(self):
+        print(self.render_board())
 
+    def render_board(self):
+        # draw board as a string, given array of pieces
+        board_str = ''
+        if self.axes:
+            board_str += 'a b c d e f g h  \n'
+        for i in range(7, -1, -1):
+            for piece in self.board[i]:
+                if piece:
+                    board_str += f'{piece} '
+                else:
+                    board_str += '_ '
+            if self.axes:
+                board_str += str(i + 1)
+            board_str += '\n'
+        return board_str
 
-def update_board():
-    print(render_board())
 
 if __name__ == '__main__':
-    update_board()
+    gameboard = Board(True)
+    gameboard.update_board()
--- test_ui.py
+++ test_ui.py
@@ -6,20 +6,29 @@ BK, BQ, BR, BB, BN, BP = '♚', '♛', '♜', '♝', '♞', '♟'
 
 
 class TestUiDrawing(unittest.TestCase):
+    def setUp(self):
+        self.test_board = chess.Board(False)
+
     def test_draw_empty_board(self):
-        self.assertEqual(chess.render_board([None]), '_ _ _ _ _ _ _ _\n' * 8)
+        row = [None] * 8
+        self.test_board.board = [row] * 8
+        self.assertEqual(self.test_board.render_board(), '_ _ _ _ _ _ _ _ \n' * 8)
 
     def test_draw_start_board(self):
-        start_board = """♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
-♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
-_ _ _ _ _ _ _ _
-_ _ _ _ _ _ _ _
-_ _ _ _ _ _ _ _
-_ _ _ _ _ _ _ _
-♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
-♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
+        start_board = """♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜ \n♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟ \n\
+_ _ _ _ _ _ _ _ \n_ _ _ _ _ _ _ _ \n_ _ _ _ _ _ _ _ \n_ _ _ _ _ _ _ _ \n\
+♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙ \n♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖ 
 """
-        self.assertEqual(chess.render_board(), start_board)
+        self.assertEqual(self.test_board.render_board(), start_board)
+
+    def test_draw_board_axes(self):
+        self.test_board.axes = True
+        row = [None] * 8
+        self.test_board.board = [row] * 8
+        axes_board = 'a b c d e f g h  \n'
+        for i in range(8, 0, -1):
+            axes_board += f'_ _ _ _ _ _ _ _ {i}\n'
+        self.assertEqual(self.test_board.render_board(), axes_board)
 
 
 if __name__ == '__main__':

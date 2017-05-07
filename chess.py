57d925234709d240445c9fdcdceaa51ac7479f59
Caro Appleby <caroline.appleby@cantab.net>
2017-05-07 18:56:29 +0100

    Some simple type hinting added

--- chess.py
+++ chess.py
@@ -2,6 +2,9 @@ import re
 import moves
 import copy
 
+# TODO custom type for board list thing
+
+COLS_MAP = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
 WK, WQ, WR, WB, WN, WP = '♔', '♕', '♖', '♗', '♘', '♙'
 BK, BQ, BR, BB, BN, BP = '♚', '♛', '♜', '♝', '♞', '♟'
 BLACK_PIECES = frozenset([BK, BQ, BR, BB, BN, BP])
@@ -12,13 +15,13 @@ MOVES_MAP = {WP: moves.white_pawn_move, BP: moves.black_pawn_move, BR: moves.roo
 
 
 class Board:
-    def __init__(self, axes):
-        self.board = [[WR, WN, WB, WQ, WK, WB, WN, WR], [WP] * 8,  [None] * 8, [None] * 8,
-                      [None] * 8, [None] * 8, [BP] * 8, [BR, BN, BB, BQ, BK, BB, BN, BR]]
-        self.axes = axes
-        self.white_move = True
+    def __init__(self, axes: bool) -> None:
+        self.board: list = [[WR, WN, WB, WQ, WK, WB, WN, WR], [WP] * 8,  [None] * 8, [None] * 8,
+                            [None] * 8, [None] * 8, [BP] * 8, [BR, BN, BB, BQ, BK, BB, BN, BR]]
+        self.axes: bool = axes
+        self.white_move: bool = True
 
-    def update_board(self, move_str=None):
+    def update_board(self, move_str=None) -> None:
         # given a user's move, update the game board
         if move_str:
             move = self.parse_move(move_str)
@@ -29,7 +32,7 @@ class Board:
                 elif (not self.white_move) and (self.board[origin[0]][origin[1]] not in BLACK_PIECES):
                     print("White move, you can't move that piece")
                 else:
-                    valid_moves = self.get_valid_moves(origin[0], origin[1])
+                    valid_moves: set = self.get_valid_moves(origin[0], origin[1])
                     if target in valid_moves:
                         self.board[target[0]][target[1]], self.board[origin[0]][origin[1]] =\
                             self.board[origin[0]][origin[1]], None
@@ -40,37 +43,38 @@ class Board:
             print('No move made')
         print(self.render_board())
 
-    def parse_move(self, move_str):
+    def parse_move(self, move_str: str):
         # expect move in form 'a8 a4' - move piece on grid ref a8 to a4
-        cols_map = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+        # or 'a4?' - what's the valid moves for the piece on a4?
         if not re.match(r'[a-h][1-8] [a-h][1-8]', move_str):
             if re.match(r'[a-h][1-8]\?', move_str):
-                x, y = int(move_str[1]) - 1, cols_map[move_str[0]]
+                x: int = int(move_str[1]) - 1
+                y: int = COLS_MAP[move_str[0]]
                 if self.board[x][y]:
-                    self.display_valid_moves(int(move_str[1]) - 1, cols_map[move_str[0]])
+                    self.display_valid_moves(int(move_str[1]) - 1, COLS_MAP[move_str[0]])
                 else:
                     print('No piece there...')
             else:
                 print('Invalid move command, please try again')
             return None
         origin, target = move_str.split(' ')
-        return (int(origin[1]) - 1, cols_map[origin[0]]), (int(target[1]) - 1, cols_map[target[0]])
+        return (int(origin[1]) - 1, COLS_MAP[origin[0]]), (int(target[1]) - 1, COLS_MAP[target[0]])
 
-    def display_valid_moves(self, row, col):
-        moves_set = self.get_valid_moves(row, col)
-        show = copy.deepcopy(self.board)
+    def display_valid_moves(self, row: int, col: int) -> None:
+        moves_set: set = self.get_valid_moves(row, col)
+        show: list = copy.deepcopy(self.board)
         for location in moves_set:
             show[location[0]][location[1]] = 'x'
         print(self.render_board(show))
 
-    def get_valid_moves(self, row, col):
+    def get_valid_moves(self, row: int, col: int) -> set:
         return MOVES_MAP[self.board[row][col]]((row, col), self.board)
 
-    def render_board(self, board=None):
+    def render_board(self, board=None)-> str:
         # draw board as a string, given array of pieces
         if not board:
-            board = self.board
-        board_str = ''
+            board: list = self.board
+        board_str: str = ''
         if self.axes:
             board_str += 'a b c d e f g h  \n'
         for i in range(7, -1, -1):
@@ -88,8 +92,8 @@ class Board:
 if __name__ == '__main__':
     print('\tPlay chess! Type your moves as "a2 a4" - move piece on a2 to a4\n\
     \tor query where you can move with "a2?"\n\tType "quit" to stop\n')
-    gameboard = Board(True)
-    move = None
-    while move != 'quit':
-        gameboard.update_board(move)
-        move = input('Your move: ')
+    gameboard: Board = Board(True)
+    enter_move = None
+    while enter_move != 'quit':
+        gameboard.update_board(enter_move)
+        enter_move = input('Your move: ')
--- moves.py
+++ moves.py
@@ -4,11 +4,13 @@ BK, BQ, BR, BB, BN, BP = '♚', '♛', '♜', '♝', '♞', '♟'
 BLACK_PIECES = frozenset([BK, BQ, BR, BB, BN, BP])
 WHITE_PIECES = frozenset([WK, WQ, WR, WB, WN, WP])
 
+# TODO custom type for valid
 
-def white_pawn_move(location, board):
+
+def white_pawn_move(location: tuple, board: list)-> set:
     # TODO en passant
-    moves_set = set()
-    pos = (location[0] + 1, location[1])
+    moves_set: set = set()
+    pos: tuple = (location[0] + 1, location[1])
     if valid(board, pos, WHITE_PIECES)[0] and (not valid(board, pos, WHITE_PIECES)[1]):
         moves_set.add(pos)
         pos = (location[0] + 2, location[1])
@@ -23,10 +25,10 @@ def white_pawn_move(location, board):
     return moves_set
 
 
-def black_pawn_move(location, board):
+def black_pawn_move(location: tuple, board: list) -> set:
     # TODO en passant
-    moves_set = set()
-    pos = (location[0] - 1, location[1])
+    moves_set: set = set()
+    pos: tuple = (location[0] - 1, location[1])
     if valid(board, pos, BLACK_PIECES)[0] and (not valid(board, pos, BLACK_PIECES)[1]):
         moves_set.add(pos)
         pos = (location[0] - 2, location[1])
@@ -41,41 +43,41 @@ def black_pawn_move(location, board):
     return moves_set
 
 
-def knight_move(location, board):
-    moves_set = set()
-    own_pieces = get_own_pieces(location, board)
+def knight_move(location: tuple, board: list) -> set:
+    moves_set: set = set()
+    own_pieces: set = get_own_pieces(location, board)
     for i in ((1, 2), (2, 1)):
         for j in [(x * i[0], y * i[1]) for x in [1, -1] for y in [1, -1]]:
-            pos = add_tuples(j, location)
+            pos: tuple = add_tuples(j, location)
             if valid(board, pos, own_pieces)[0]:
                 moves_set.add(pos)
     return moves_set
 
 
-def rook_move(location, board):
+def rook_move(location: tuple, board: list) -> set:
     # TODO castling
     return move_all_directions(location, board, [(1, 0), (-1, 0), (0, 1), (0, -1)])
 
 
-def bishop_move(location, board):
+def bishop_move(location: tuple, board: list) -> set:
     return move_all_directions(location, board, [(x, y) for x in [1, -1] for y in [1, -1]])
 
 
-def queen_move(location, board):
+def queen_move(location: tuple, board: list) -> set:
     return bishop_move(location, board).union(rook_move(location, board))
 
 
-def king_move(location, board):
+def king_move(location: tuple, board: list) -> set:
     # TODO check
     # TODO castling
     return move_all_directions(location, board, [(x, y) for x in [1, -1, 0] for y in [1, -1, 0]])
 
 
-def move_all_directions(location, board, dirs):
-    moves_set = set()
+def move_all_directions(location: tuple, board: list, dirs: list) -> set:
+    moves_set: set = set()
     for d in dirs:
-        allowed = True
-        pos = add_tuples(d, location)
+        allowed: bool = True
+        pos: tuple = add_tuples(d, location)
         while allowed:
             check = valid(board, pos, get_own_pieces(location, board))
             if check[0]:
@@ -88,20 +90,20 @@ def move_all_directions(location, board, dirs):
     return moves_set
 
 
-def get_own_pieces(location, board):
+def get_own_pieces(location: tuple, board: set) -> set:
     if board[location[0]][location[1]] in WHITE_PIECES:
         return WHITE_PIECES
     else:
         return BLACK_PIECES
 
 
-def valid(board, pos, bad_pieces):
+def valid(board: set, pos: tuple, bad_pieces: set):
     if 0 <= pos[0] < 8 and 0 <= pos[1] < 8:
         if (board[pos[0]][pos[1]] is None) or (board[pos[0]][pos[1]] not in bad_pieces):
             return True, board[pos[0]][pos[1]]
     return False, None
 
 
-def add_tuples(a, b):
+def add_tuples(a: tuple, b: tuple) -> tuple:
     return tuple([sum(x) for x in zip(a, b)])
 
